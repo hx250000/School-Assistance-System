97c49bbee9d4b4afdbafdf8223c7c430dd582689
姚益多 <1675462@qq.com>
2026-03-21 19:15:10 +0800

    初步做完两个页面的前端，初始化前端框架

---
+++ frontend/campusTask/app/src/main/java/com/example/campustask/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.example.campustask
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val bottomNav = findViewById<BottomNavigationView>(R.id.bottom_nav)
+
+        // 👉 默认加载首页（关键！）
+        if (savedInstanceState == null) {
+            loadFragment(HomeFragment())
+        }
+
+        // 👉 点击底部栏切换页面
+        bottomNav.setOnItemSelectedListener {
+            when (it.itemId) {
+
+                R.id.nav_home -> loadFragment(HomeFragment())
+
+                R.id.nav_publish -> loadFragment(PublishFragment())
+//
+//                R.id.nav_task -> loadFragment(TaskFragment())
+//
+//                R.id.nav_shop -> loadFragment(ShopFragment())
+//
+//                R.id.nav_profile -> loadFragment(ProfileFragment())
+            }
+            true
+        }
+    }
+
+    private fun loadFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragment_container, fragment)
+            .commit()
+    }
+}
---
+++ frontend/campusTask/app/src/main/java/com/example/campustask/Task.kt
@@ -0,0 +1,9 @@
+package com.example.campustask
+
+data class Task(
+    val title: String,
+    val people: String,
+    val score: String,
+    val time: String,
+    val category: String
+)
