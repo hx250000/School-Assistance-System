6e9b6a83af5ca79e882774abe2c5c7ee2b2b9624
sklayan <sklayan852.gmail.com>
2026-03-31 13:00:48 +0800

    add backend code

--- backend/back/src/main/java/org/example/back/dto/request/TaskCreateRequest.java
+++ backend/back/src/main/java/org/example/back/dto/request/TaskCreateRequest.java
@@ -1,10 +1,14 @@
 package org.example.back.dto.request;
+import lombok.Data;
 
+@Data
 public class TaskCreateRequest {
     private String title;
     private String description;
     private String type;
+    private Long publisherId;      // ✅ 补上
     private Integer needPeople;
     private Integer rewardPoints;
+    private Double rewardMoney;    // ✅ 补上
     private String deadline;
 }
