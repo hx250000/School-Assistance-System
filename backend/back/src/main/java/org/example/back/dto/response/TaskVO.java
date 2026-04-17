1528fdc7434c5067864624522bbc8128d8dae1f1
Xing He <1186733874@qq.com>
2026-04-17 12:04:42 +0800

    修改后端TaskVO类，与前端对应

--- backend/back/src/main/java/org/example/back/dto/response/TaskVO.java
+++ backend/back/src/main/java/org/example/back/dto/response/TaskVO.java
@@ -1,87 +1,24 @@
 package org.example.back.dto.response;
 
+import lombok.Data;
 
+import java.math.BigDecimal;
+
+@Data
 public class TaskVO {
 
     private Long id;
-
     private String title;
-
     private String description;
-
     private Integer needPeople;
-
     private Integer currentPeople;
-
+    private String type;
     private String status;
-
     private Long publisherId;
 
-    private String createTime;
+    private Integer rewardPoints;
+//    private BigDecimal rewardMoney;
 
-    // ===== getter / setter =====
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public Integer getNeedPeople() {
-        return needPeople;
-    }
-
-    public void setNeedPeople(Integer needPeople) {
-        this.needPeople = needPeople;
-    }
-
-    public Integer getCurrentPeople() {
-        return currentPeople;
-    }
-
-    public void setCurrentPeople(Integer currentPeople) {
-        this.currentPeople = currentPeople;
-    }
-
-    public String getStatus() {
-        return status;
-    }
-
-    public void setStatus(String status) {
-        this.status = status;
-    }
-
-    public Long getPublisherId() {
-        return publisherId;
-    }
-
-    public void setPublisherId(Long publisherId) {
-        this.publisherId = publisherId;
-    }
-
-    public String getCreateTime() {
-        return createTime;
-    }
-
-    public void setCreateTime(String createTime) {
-        this.createTime = createTime;
-    }
+    private Long deadline;
+    private Long createdAt;
 }
